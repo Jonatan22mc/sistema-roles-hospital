4c42eabe67394203dcc9e7acc923751b6d6354ff
Jonatan Elias Meza Choque <jonatan22mc@gmail.com>
2026-08-24 10:37:31 -0500

    feat: implementar login directo, gestion de usuarios y perfiles con soft delete

--- src/autenticacion/dto/update-autenticacion.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateAutenticacionDto } from './create-autenticacion.dto';
-
-export class UpdateAutenticacionDto extends PartialType(CreateAutenticacionDto) {}
